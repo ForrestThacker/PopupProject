a1fa4f8427a8feddf8ea507bf49ebdfc3ff99992
ForrestThacker <ftha4681@740s-pl-116.local>
2015-09-24 12:39:57 -0600

    Added a controller.

--- src/input/controller/PopupController.java
+++ src/input/controller/PopupController.java
@@ -1,9 +1,13 @@
 package input.controller;
 
+import input.view.PopupDisplay;
+
 public class PopupController 
 {
+	private PopupDisplay myPopups;
+	
 	public void start()
 	{
-		
+		myPopups.displayResponse("Words are here");
 	}
 }
