f55b1365fc1dd8f5f05e84ba55864b2e9149e591
ForrestThacker <ftha4681@740s-pl-116.local>
2015-09-24 12:35:50 -0600

    Added a display class.

---
+++ src/input/controller/Popuprunner.java
@@ -0,0 +1,13 @@
+package input.controller;
+
+public class Popuprunner 
+{
+	public void main (String [] args)
+	{
+		//Something my Something = new Something();
+		//mySomething.start();
+		PopupController myPopupController = new PopupController();
+		myPopupController.start();
+	}
+	
+}
